11eb35f9d1764dfb91ea1e2d73f0592e9113eeaa
Abhijit Ghosh <gbubun@gmail.com>
2026-01-27 22:27:20 +0530

    added and committed the test file to check openai api connectivity

---
+++ test.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+# Load environment variables from .env
+load_dotenv(override=True)
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Check if the keys are loaded correctly
+if openai_api_key: 
+    print(f"OPENAI API key exists and begins with : {openai_api_key[:10]}...")
+else:
+    print("Failed to load openai_api_key.")
+
+
+
+#use case 
+query = """Tell me about Riemann Curvature tensor in two lines"""
+
+messages = [{"role":"user", "content":query}]
+
+#OpenAI part 
+#Connect to OpenAI client library 
+openai = OpenAI(api_key=openai_api_key)
+response_openai = openai.chat.completions.create(messages=messages,model="gpt-4.1-mini")
+print("OpenAI response:")
+print(response_openai.choices[0].message.content)
+
+
+
+
+
+
+
+
+
